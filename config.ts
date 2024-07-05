c358eb965c8eebb977cd5fcab1f3c77bb9c29231
Edy <edigleyssonsilva@gmail.com>
2024-07-04 21:22:36 -0300

    export firebaseConfig

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const _firebaseConfig = {
+export const firebaseConfig = {
   // Your web app's Firebase configuration here
   // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
   apiKey: 'API_KEY',
